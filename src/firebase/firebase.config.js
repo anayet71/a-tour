1999ebbdaf28cec183f6b76c853caf339a96164e
anayet71 <ariyananayet@gmail.com>
2025-04-18 14:21:07 +0600

    Set up Firebase Project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwcN85qCniBXZhHwGncOYVcbN7X4KQVp4",
+  authDomain: "a-travel-3700d.firebaseapp.com",
+  projectId: "a-travel-3700d",
+  storageBucket: "a-travel-3700d.firebasestorage.app",
+  messagingSenderId: "986741380086",
+  appId: "1:986741380086:web:9f32799898738cf18a6dd7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app 
